bff2cbe95cd3fbca74de2e770f051fb1fcd59878
Juan Cabria <jjcabria0721@gmail.com>
2025-04-17 22:27:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jitcall',
+  webDir: 'www'
+};
+
+export default config;
